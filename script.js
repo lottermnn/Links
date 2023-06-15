93c8104cac2d77d47e579988fd9cdbca89ca2595
Eduardo <eduardolottermann520@gmail.com>
2023-06-15 16:35:31 -0300

    New's

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle("light")
+  
+    //pegar a tag imag
+    const img = document.querySelector("#profile img")
+  
+    //substituir a imagem
+    if (html.classList.contains("light")) {
+      //se tiver light mode, adicionar a imagem light
+      //avatar escuro
+      img.setAttribute("src", "./assets/Avatar-light.png")
+      img.setAttribute("alt", "Fofo secund")
+    } else {
+      //se tiver sem light mode, mandar a imagem nomarl
+      //trocar avatar
+      img.setAttribute("src", "./assets/Avatar1.png")
+      img.setAttribute("alt", "Fofo prime")
+    }
+  }
